e826395e9aae7e4ee1a1d557be7fcaf540ac7adf
powdersnow <gundam6324@gmail.com>
2025-04-28 02:28:03 +0900

    Add uv

---
+++ tools/install_uv.sh
@@ -0,0 +1,11 @@
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# installing to /home/powdersnow/.local/bin
+#   uv
+#   uvx
+# everything's installed!
+
+# To add $HOME/.local/bin to your PATH, either restart your shell or run:
+
+#     source $HOME/.local/bin/env (sh, bash, zsh)
+#     source $HOME/.local/bin/env.fish (fish)
